1ab7529df6e5696f548b0f0afb4a0651d2afbdab
Yuuki Osada <78744619+Myxogastria0808@users.noreply.github.com>
2022-11-20 00:32:42 +0900

    Hello

--- features.js
+++ features.js
@@ -1,3 +1,4 @@
+//features
 var $poster = $('#card-container'),
   $shine = $('.shine'),
   w = $(window).width(),
@@ -20,3 +21,20 @@ $(window).on('mousemove', function(e) {
 
   $poster.css('transform', transformPoster);
 });
+
+//header
+var _window = $(window),
+    _header = $('.header'),
+    heroBottom;
+ 
+_window.on('scroll',function(){
+    heroBottom = $('.header_animation_indent').height();
+    if(_window.scrollTop() > heroBottom){
+        _header.addClass('transform');   
+    }
+    else{
+        _header.removeClass('transform');   
+    }
+});
+ 
+_window.trigger('scroll');
