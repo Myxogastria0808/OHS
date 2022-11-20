a3f2b9c43b30b296ea9471ac2e5d9f67827cde2a
Yuuki Osada <78744619+Myxogastria0808@users.noreply.github.com>
2022-11-20 21:23:47 +0900

    Hello

--- 学校紹介/script.js
+++ 学校紹介/script.js
@@ -25,3 +25,91 @@ function onClick(e) {
 	
 	figure.style.transform = `rotateY(${currImage * -theta}rad)`;
 }
+
+//button
+$('.container-btn').on('click', '.btn-modal', function(e){
+	e.preventDefault();
+	if ( $(this).hasClass('play') ) {
+		$(this).removeClass('play');
+		$(this).addClass('pause');
+	} else {
+		$(this).removeClass('pause');
+		$(this).addClass('play');
+	}
+});
+
+//audio
+var wavesurfer = WaveSurfer.create({
+	container: '#waveform',
+	  // waveColor: '#137a7f',
+	  // progressColor: 'purple',
+	  waveColor: [ // an array of colors, to be applied as gradient color stops to the waveform.
+	  "red",
+	  "green",
+	  "purple",
+	  "yellow",
+	  "rgba(0,255,255,.5)",
+	],
+	  progressColor: [ // the gradient fill styles are also available on the progressColor option
+	  "orange",
+	  "blue",
+	  "cyan",
+	  "black",
+	  "rgba(0,255,255,.5)",
+	],
+	  backgroundColor: 'skyblue',
+	  audioRate: 1,
+	  splitChannels: false,
+	  normalize: true,
+	  autoCenter: true,
+	  barWidth: 3,
+	  barGap: 3,
+	  barRadius: 3,
+	  cursorColor: 'green',
+	  cursorWidth: 5,
+	  forceDecode: true,
+	  height: 300,
+	  hideCursor: false,
+	  minPxPerSec: 1,
+	  partialRender: true,
+	  responsive: 0.01,
+	  plugins: [
+		WaveSurfer.cursor.create({
+			showTime: true,
+			opacity: 1,
+			customShowTimeStyle: {
+				'background-color': '#000',
+				color: '#fff',
+				padding: '2px',
+				'font-size': '10px'
+			}
+		}),
+		WaveSurfer.markers.create({
+		  markers: [
+			  {
+				  time: 1.0,
+				  label: "V1",
+				  color: '#ff990a'
+			  },
+			  {
+				  time: 5.0,
+				  label: "V2",
+				  color: '#00ffcc',
+				  position: 'top'
+			  }
+		  ]
+		})
+	],
+  });
+  
+  wavesurfer.load('./onoko-koka.mp3');
+  
+  playstop = () => {
+	if (wavesurfer.isPlaying()){  
+	  // wavesurfer.setCurrentTime(0)
+	  wavesurfer.playPause();
+	}
+	else{
+	  wavesurfer.play();
+	}
+  };
